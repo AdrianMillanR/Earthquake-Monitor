8b1f7b63b14b4c03613d87e46901b75abf1f5fb0
Adrian Millan <adrian231998@gmail.com>
2021-01-11 13:49:52 -0600

    implementamos uso de DataBinding en nuestro Adapter

--- app/src/main/java/com/adrian/earthquakemonitor/EqAdapter.kt
+++ app/src/main/java/com/adrian/earthquakemonitor/EqAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.adrian.earthquakemonitor.databinding.EqListItemBinding
 
 class  EqAdapter : ListAdapter<Earthquake, EqAdapter.EqViewHolder>(DiffCallback)  {
 
@@ -21,19 +22,26 @@ class  EqAdapter : ListAdapter<Earthquake, EqAdapter.EqViewHolder>(DiffCallback)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EqAdapter.EqViewHolder {
-        val view= LayoutInflater.from(parent.context).inflate(R.layout.eq_list_item,parent,false)
-        return EqViewHolder(view)
+        val binding= EqListItemBinding.inflate(LayoutInflater.from(parent.context))
+        return EqViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: EqAdapter.EqViewHolder, position: Int) {
         val earthquake= getItem(position)
-        holder.magnitudeText.text= earthquake.magnitude.toString()
-        holder.placeText.text= earthquake.place
+        holder.bind(earthquake)
     }
 
-    inner class EqViewHolder(val view:View): RecyclerView.ViewHolder(view){
-        val magnitudeText= view.findViewById<TextView>(R.id.magnitude_text)
-        val placeText= view.findViewById<TextView>(R.id.place_text)
+    inner class EqViewHolder(private val binding: EqListItemBinding): RecyclerView.ViewHolder(binding.root){
+
+
+        fun bind(earthquake: Earthquake){
+            binding.magnitudeText.text=earthquake.magnitude.toString()
+            binding.placeText.text=earthquake.place
+
+        }
+
     }
+
+
 
 }
