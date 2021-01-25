8b0916eab9dd75c9fcdadd974f416cd808b13c48
Adrian Millan <adrian231998@gmail.com>
2021-01-25 14:38:47 -0600

    Creamos una corutina en nuestro ViewModel, para que cuando codifiquemos la obtención de los datos de internet con Retrofit se realice en segundo plano y no en el hilo principal de la aplicación

--- app/src/main/java/com/adrian/earthquakemonitor/MainViewModel.kt
+++ app/src/main/java/com/adrian/earthquakemonitor/MainViewModel.kt
@@ -3,6 +3,10 @@ package com.adrian.earthquakemonitor
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainViewModel: ViewModel() {
     private var _eqList= MutableLiveData<MutableList<Earthquake>>()
@@ -10,19 +14,24 @@ class MainViewModel: ViewModel() {
         get() = _eqList
 
     init {
-        fetchEarthquakes()
+        viewModelScope.launch {
+            _eqList.value= fetchEarthquakes()
+        }
     }
 
-    private fun fetchEarthquakes() {
-        val eqList= mutableListOf<Earthquake>()
-        eqList.add(Earthquake("1","Ciudad de México",4.3,273847152L,-102.4756,28.4748))
-        eqList.add(Earthquake("2","Buenos Aires",1.8,273847152L,-102.4756,28.4748))
-        eqList.add(Earthquake("3","Madrid",3.6,273847152L,-102.4756,28.4748))
-        eqList.add(Earthquake("4","Uta",5.6,273847152L,-102.4756,28.4748))
-        eqList.add(Earthquake("5","Roma",1.2,273847152L,-102.4756,28.4748))
-        eqList.add(Earthquake("6","Tokio",7.3,273847152L,-102.4756,28.4748))
+    private suspend fun fetchEarthquakes(): MutableList<Earthquake> {
+        return withContext(Dispatchers.IO){
+            val eqList= mutableListOf<Earthquake>()
+            eqList.add(Earthquake("1","Ciudad de México",4.3,273847152L,-102.4756,28.4748))
+            eqList.add(Earthquake("2","Buenos Aires",1.8,273847152L,-102.4756,28.4748))
+            eqList.add(Earthquake("3","Madrid",3.6,273847152L,-102.4756,28.4748))
+            eqList.add(Earthquake("4","Uta",5.6,273847152L,-102.4756,28.4748))
+            eqList.add(Earthquake("5","Roma",1.2,273847152L,-102.4756,28.4748))
+            eqList.add(Earthquake("6","Tokio",7.3,273847152L,-102.4756,28.4748))
+            eqList
+        }
 
-        _eqList.value=eqList
+
     }
 
 }
