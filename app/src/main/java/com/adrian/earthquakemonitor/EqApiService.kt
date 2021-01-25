2ebf34868256b602832104fbb8d94d284c188aa2
Adrian Millan <adrian231998@gmail.com>
2021-01-25 12:39:28 -0600

    agregamos dos dependencias de la libreria Retrofit, la cual nos permitirá consultar una API con datos de los ultimos terremotos en el mundo, creamos una interfaz a través de la cual hacemos los request a la API, tambien instanciamos un objeto de la clase ya predefinida Retrofit, en la cual indicamos el url de la API que queremos consumir

---
+++ app/src/main/java/com/adrian/earthquakemonitor/EqApiService.kt
@@ -0,0 +1,18 @@
+package com.adrian.earthquakemonitor
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+
+interface EqApiService {
+    @GET("")
+    fun getLastHourEarthquakes(): String
+}
+
+private var retrofit = Retrofit.Builder()
+    .baseUrl("https://earthquake.usgs.gov/fdsnws/event/1/ ")
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .build()
+
+var service: EqApiService = retrofit.create<EqApiService>(EqApiService::class.java)
