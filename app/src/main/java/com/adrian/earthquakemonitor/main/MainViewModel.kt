6277a790882969095bacf698dd9e5530a576d8a4
Adrian Millan <adrian231998@gmail.com>
2021-02-02 13:10:41 -0600

    agregamos un Try Catch para que cuando no haya internet, en lugar de crashear la app, utilice los datos que tiene en la Base de datos local para mostrarlos en el Recycler View

--- app/src/main/java/com/adrian/earthquakemonitor/main/MainViewModel.kt
+++ app/src/main/java/com/adrian/earthquakemonitor/main/MainViewModel.kt
@@ -1,11 +1,15 @@
 package com.adrian.earthquakemonitor.main
 
 import android.app.Application
+import android.nfc.Tag
+import android.util.Log
 import androidx.lifecycle.*
 import com.adrian.earthquakemonitor.Earthquake
 import com.adrian.earthquakemonitor.database.getDatabase
 import kotlinx.coroutines.launch
+import java.net.UnknownHostException
 
+private  val TAG = MainViewModel::class.java.simpleName
 class MainViewModel(application: Application): AndroidViewModel(application) {
     private val database= getDatabase(application)
     private val repository= MainRepository(database)
@@ -14,7 +18,12 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
 
     init {
         viewModelScope.launch {
-            repository.fetchEarthquakes()
+            try {
+                repository.fetchEarthquakes()
+            } catch (e: UnknownHostException){
+                Log.d(TAG,"No internet conection.", e)
+            }
+
         }
     }
 
